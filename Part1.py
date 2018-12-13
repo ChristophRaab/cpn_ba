ce8ca3fd06260c29a1a99eefe372a19f686d8985
Sebastian <sebb@sebb767.de>
2018-12-13 16:29:38 +0100

    Refactored Part1 to have the sklearn api

--- Part1.py
+++ Part1.py
@@ -49,24 +49,14 @@ def load_dataset(path):
     return dog_files, dog_targets
 
 
-def mapClassToIndex(arr):
-    for i in range(len(arr)):
-        if arr[i] > 0.99:
-            return i
-    return 0
-
-
-def mapClassArrayToIndexArray(arr):
-    return list(map(lambda x: mapClassToIndex(x), arr))
-
-
 class LvqParams:
-    def __init__(self, sigma, prototypes_per_class, epochs, batch_size):
+    def __init__(self, sigma, prototypes_per_class, epochs, batch_size, mean_dimensions=None):
         self.sigma = sigma
         self.prototypes_per_class = prototypes_per_class
         self.epochs = epochs
         self.batch_size = batch_size
         self.cost = self.computeCost()
+        self.mean_dimensions = mean_dimensions
 
     def computeCost(self):
         """
@@ -90,39 +80,153 @@ class LvqParams:
         Creates an instance of the RSLVQ implementation using the given parameters
         :return:
         """
-        return nRSLVQ(
+        return LvqClassifierLayer(
             prototypes_per_class=self.prototypes_per_class,
             sigma=self.sigma,
             batch_size=self.batch_size,
-            n_epochs=self.epochs
+            epochs=self.epochs,
+            mean_dimensions=self.mean_dimensions
         )
+
+    def test_lvq(self, train_set, train_targets, test_set, test_targets, description=""):
+        """
+        Tests an LVQ implementation with the given training and validation data.
+        :param test_targets: The actual classification for the test data.
+        :param test_set: The test samples.
+        :param train_targets: The correct classification for the training samples.
+        :param train_set: The training samples.
+        :param description: A string describing the LVQ implementation.
+        :return: A tuple of (description, correct count, total count, wrong count, accurary percentage)
+        """
+        lvq = self.createRslvq()
+        fitdata = lvq.fit(train_set, train_targets)
+
+        predict = lvq.predict(test_set)
+
+        diff = list(map(lambda x: 1 if x == 0 else 0, predict - test_targets))
+        total = len(diff)
+        correct = sum(diff)
+        wrong = total - correct
+
+        if description != "":
+            description = "[%s] " % description
+
+        results = (description, correct, total, wrong, (correct / total) * 100)
+
+        return results
 
     def __lt__(self, other):
         return self.cost < other.cost
 
 
+class DimensionReduction:
+    """
+    This class provides dimensional reduction for input data.
+    """
+
+    def __init__(self, filter='flatten', mean_dimensions=None, pca_dims=32):
+        """
+        Initializes the dimension reduction.
+        :param filter:
+        The function to use for reducing the data dimensions to two. Can be 'min', 'max', 'mean', 'pca' or 'flatten'.
+        :param mean_dimensions:
+        The dimensions to reduce. For example, if one has a 4-dimensional array and the given value is (1,2) the new
+        array will have the shape [old.shape[0],old.shape[3]]. Ignored for filter='flatten'. Defaults to old.shape[2:].
+        :param pca_dims: The dimensions to use when using a pca.
+        """
+        self.mean_dimensions = mean_dimensions
+        self.pca_dims = pca_dims
+        self.filters = {
+            "pca": self._pca_data,
+            "min": self._min_data,
+            "max": self._max_data,
+            "mean": self._mean_data,
+            "flatten": self._flatten_data
+        }
+
+        if filter not in self.filters:
+            raise ValueError("Unknown filter function '%s'!" % filter)
+        self.filter = self.filters[filter]
+
+    def refine(self, data):
+        """
+        Reduces the multi-dimensional input data to a two-dimensional array with the given filter.
+        :return: The refined data
+        """
+        shapelen = len(data.shape)
+
+        if shapelen < 2:
+            raise ValueError("Input data has too few dimensions!")
+        elif shapelen == 2:
+            return data
+
+        mean_dim = self.mean_dimensions
+        if mean_dim is None:
+            # no shape given; simply remove all extra dimensions
+            mean_dim = data.shape[2:]
+        elif len(mean_dim) != shapelen - 2:
+            raise ValueError("The mean dimensions do not match the input!")
+
+        return self.filter(data, mean_dim)
+
+    def _mean_data(self, data, mean_axis):
+        """
+        Means the multidimensional data.
+        :return: The refined data
+        """
+        return np.mean(data, axis=mean_axis)
+
+    def _max_data(self, data, mean_axis):
+        """
+        Uses max to reduce the dimensions of the data.
+        :return: The refined data
+        """
+        return np.max(data, axis=mean_axis)
+
+    def _min_data(self, data, mean_axis):
+        """
+        Uses min to reduce the dimensions of the data.
+        :return: The refined data
+        """
+        return np.min(data, axis=mean_axis)
+
+    def _pca_data(self, flattened):
+        """
+        Uses a PCA to reduce the data dimensions.
+        :param flattened: The input data.
+        :return:
+        """
+        # flatten all the input data for each sample
+        dim1size = flattened.shape[0]
+        flattened = flattened.reshape(dim1size, -1)
+        # norm the input features
+        pca = deco.PCA(self.pca_dims)
+        pca.fit(flattened[0:200])
+        return pca.transform(flattened)
+
+    def _flatten_data(self, features, mean_axis=None):
+        """
+        Flattens all sample data to a single two-dimensional array without any processing.
+        :param features: The input features
+        """
+        return features.reshape(features.shape[0], -1)
+
+
 class LvqTester:
-    def __init__(self, path="CodeData", big_features=True, filter='pca', pca_dims=137):
+    def __init__(self, path="CodeData", big_features=True, filter='flatten', pca_dims=137):
         """
         Initializes the class (and directly starts loading data)
         :param path: The path to the data set.
         :param big_features: Whether to use big feature inputs (mean on axis 1,2 instead of 2,3)
         """
         self.mean_axis = (1, 2) if big_features else (2, 3)
-        self.pca_dims = pca_dims
-        self.filters = {
-            "pca": self.pcaTrainingData,
-            "min": self.minTrainingData,
-            "max": self.maxTrainingData,
-            "mean": self.meanTrainingData,
-            "flatten": self.flattenTrainingDaten
-        }
-        self.filter = self.filters[filter]
+        self.filter = DimensionReduction(filter, self.mean_axis, pca_dims)
+
         self.bottleneck_features = np.load(path + '/DogXceptionData.npz')
 
-        self.train_set = self.refineTrainingData(self.bottleneck_features['train'])
-        self.valid_set = self.refineTrainingData(self.bottleneck_features['valid'])
-        self.test_set = self.refineTrainingData(self.bottleneck_features['test'])
+        self.train_set = self.filter.refine(self.bottleneck_features['train'])
+        self.valid_set = self.filter.refine(self.bottleneck_features['valid'])
+        self.test_set = self.filter.refine(self.bottleneck_features['test'])
 
         self.train_files, self.train_targets = load_dataset(path + '/dogImages/train')
         self.valid_files, self.valid_targets = load_dataset(path + '/dogImages/valid')
@@ -137,87 +241,20 @@ class LvqTester:
         self.lock = threading.Lock()
         print("Prepared Tester: filter=%s big_features=%s pca_dims=%d" % (filter, str(big_features), pca_dims))
 
-    def refineTrainingData(self, features):
-        """
-        Refines the multidimensional bottleneck features for use with RSLVQ.
-        :return: The refined data
-        """
-        return self.filter(features)
-
-    def meanTrainingData(self, data):
-        """
-        Means the multidimensional bottleneck features.
-        :return: The refined data
-        """
-        return np.mean(data, axis=self.mean_axis)
-
-    def maxTrainingData(self, data):
-        """
-        Uses max to reduce the dimensions of the bottleneck features.
-        :return: The refined data
-        """
-        return np.max(data, axis=self.mean_axis)
-
-    def minTrainingData(self, data):
-        """
-        Uses min to reduce the dimensions of the bottleneck features.
-        :return: The refined data
-        """
-        return np.min(data, axis=self.mean_axis)
-
-    def pcaTrainingData(self, flattened):
-        """
-        Uses a PCA to reduce the data dimensions.
-        :param features: The input data.
-        :param out_dims:
-        :return:
-        """
-        # flatten all the input data for each sample
-        dim1size = flattened.shape[0]
-        flattened = flattened.reshape(dim1size, -1)
-        # norm the input features
-        pca = deco.PCA(self.pca_dims)
-        pca.fit(flattened[0:200])
-        return pca.transform(flattened)
-
-    def flattenTrainingDaten(self, features):
-        """
-        Flattens all sample data to a single two-dimensional array without any processing.
-        :param features: The input features
-        """
-        return features.reshape(features.shape[0], -1)
-
     def mapTargetsToIndexed(self, targets):
         """
-        Maps NN targets (1 at the index n) to numerical targets (n)
+        Maps neural network targets (1 at the index n) to numerical targets (n).
         :param targets: The targets to map
         :return: A numpy array with mapped targets
         """
-        return np.array(mapClassArrayToIndexArray(targets))
+        def mapClassToIndex(arr):
+            for i in range(len(arr)):
+                if arr[i] > 0.99:
+                    return i
+            raise ValueError('No class found in array!')
 
-    def testLvq(self, lvq, descr=""):
-        """
-        Tests an LVQ implementation with the given training data and prints the results
-        :param descr: A string describing the LVQ implementation.
-        :param lvq: The LVQ implementation. Must have a fit and a predict method.
-        :return: A tuple of (description, correct count, total count, wrong count, accurary percentage)
-        """
-        fitdata = lvq.fit(self.train_set, self.train_targets_indexed)
-
-        predict = lvq.predict(self.test_set)
-
-        diff = list(map(lambda x: 1 if x == 0 else 0, predict - self.test_targets_indexed))
-        total = len(diff)
-        correct = sum(diff)
-        wrong = total - correct
-
-        if descr != "":
-            descr = "[%s] " % descr
-
-        results = (descr, correct, total, wrong, (correct/total) * 100)
-
-        print('%sCorrectly classified %d out of %d samples (%d wrong) => Accuracy of %f%%' % results)
-        return results
+        # return np.array(list(map(lambda x: mapClassToIndex(x), targets)))
+        return np.array([mapClassToIndex(sample) for sample in targets])
 
     def frange(self, x, y, jump):
         """
@@ -231,18 +268,18 @@ class LvqTester:
             yield x
             x += jump
 
-    def createTestScenarios(self):
+    def createTestScenarios(self, mean_dims=None):
         """
         Creates a set of test parameters to check for.
         :return: A list of test parameters, sorted by cost
         """
-        rslvq = nRSLVQ(prototypes_per_class=10, sigma=0.5, batch_size=200, n_epochs=20)
         tests = []
         for sigma in [0.2, 0.5, 1.0, 3.0, 5.0, 6.0]:               # 6 steps
             for batch_size in [1, 8, 16, 32, 64, 128, 256]:        # 7 steps
                 for epochs in [1, 2, 4, 8, 12]:                    # 6 steps
                     for prototypes in [1, 2, 3, 4, 6, 8, 10, 12]:  # 8 steps
-                        tests.append(LvqParams(sigma, prototypes, epochs, batch_size))  # = about 1,9k tests
+                        # about 1,9k tests
+                        tests.append(LvqParams(sigma, prototypes, epochs, batch_size, mean_dimensions=mean_dims))
 
         return sorted(tests)
 
@@ -310,7 +347,9 @@ class LvqTester:
                 self.lock.release()
 
             print("Thread %d -- starting [%s]" % (num, str(lvq)))
-            dt = self.testLvq(lvq.createRslvq(), str(lvq))
+            dt = lvq.test_lvq(
+                self.train_set, self.train_targets_indexed, self.test_set, self.test_targets_indexed, str(lvq))
+            print('%sCorrectly classified %d out of %d samples (%d wrong) => Accuracy of %f%%' % dt)
 
             self.lock.acquire()
             if isqueue:
@@ -320,12 +359,74 @@ class LvqTester:
             self.lock.release()
 
 
+class LvqClassifierLayer:
+    def __init__(self, filter='flatten', mean_dimensions=None, sigma=0.2, prototypes_per_class=8, batch_size=256, epochs=4):
+        """
+        Initializes the RSLVQ training layer.
+        :param filter:
+        The function to use for reducing the data dimensions to two. Can be 'flatten' (default), 'min', 'max' or 'mean'.
+        :param mean_dimensions:
+        The dimensions to reduce. For example, if one has a 4-dimensional array and the given value is (1,2) the new
+        array will have the shape [old.shape[0],old.shape[3]]. Ignored for filter='flatten'. Defaults to old.shape[2:].
+        :param sigma: The sigma to use for the RSLVQ classifier.
+        :param prototypes_per_class: The number of prototypes used for each class in the RSLVQ classifier.
+        :param batch_size: The batch size used in the RSLVQ classifier.
+        :param epochs: The number of training epochs.
+        """
+        if filter not in ['min', 'max', 'mean', 'flatten']:
+            raise ValueError("Unknown filter function '%s'!" % filter)
+        self.filter = DimensionReduction(filter, mean_dimensions)
+        self.sigma = sigma
+        self.prototypes_per_class = prototypes_per_class
+        self.batch_size = batch_size
+        self.epochs = epochs
+
+        self.lvq = nRSLVQ(
+            prototypes_per_class=self.prototypes_per_class,
+            sigma=self.sigma,
+            batch_size=self.batch_size,
+            n_epochs=self.epochs
+        )
+
+    def fit(self, data, classification):
+        """
+        Trains the classifier on the given data.
+        :param data:
+        The data to train on. This must be an at least two-dimensional array where each element of the first dimension
+        represents the (possibly multidimensional) data for a sample.
+        :param classification:
+        The classification for each sample of the first array. This must be an one-dimensional array where is element is
+        an int representing the class of the sample at the same index in the data array. The length of this array must
+        match the length of the data array.
+        """
+        if len(data) != len(classification):
+            raise ValueError('The provided classification data does not match the training data!')
+
+        if len(data.shape) < 2:
+            raise ValueError('The data array has too few dimensions!')
+
+        data = self.filter.refine(data)
+        self.lvq.fit(data, classification)
+
+    def predict(self, data):
+        """
+        Predicts the classes for the given input data.
+        :param data: The data to analyze.
+        :return: An one-dimensional array where each entry contains the predicted class for the input sample.
+        """
+        if len(data.shape) < 2:
+            raise ValueError('The data array has too few dimensions!')
+
+        data = self.filter.refine(data)
+        return self.lvq.predict(data)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Run RSLVQ learning accuracy tests on the Xception bottleneck feature dataset.')
     parser.add_argument('--threads', '-t', type=int, default=2, help='Number of threads to use.')
     parser.add_argument('--code-path', '-p', type=str, default='CodeData', help='Path to the learning dataset.')
-    parser.add_argument('--filter', '-f', type=str, choices=['min', 'max', 'mean', 'pca', 'flatten'], default='mean',
+    parser.add_argument('--filter', '-f', type=str, choices=['min', 'max', 'mean', 'pca', 'flatten'], default='flatten',
                         help='How to prepare the bottleneck features before using them with the LVQ algorithm.')
     parser.add_argument('--big-features', action='store_true',
                         help='Whether to mean the input to big features or small ones.')
@@ -338,8 +439,8 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     tester = LvqTester(big_features=args.big_features, path=args.code_path, filter=args.filter, pca_dims=args.pca_dims)
-    # tests = tester.createTestScenarios()
-    tests = [ LvqParams(sigma=.2, prototypes_per_class=8, batch_size=256, epochs=4) ]
+    # tests = tester.createTestScenarios(mean_dims=tester.mean_axis)
+    tests = [ LvqParams(sigma=.2, prototypes_per_class=8, batch_size=256, epochs=4, mean_dimensions=tester.mean_axis) ] * 2
     print('Running %d tests with%s big feature space and %d %s.'
           % (len(tests), "out" if not args.big_features else "", args.threads, "processes" if args.use_mp else "threads"))
     tester.runTestsParallel(tests, threads=args.threads, use_multiprocessing=args.use_mp)
