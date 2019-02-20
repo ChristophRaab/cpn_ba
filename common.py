63e8a4230f6060987acfd564c0333a650457a45b
Sebastian <sebb@sebb767.de>
2019-02-20 01:50:05 +0100

    Ported common functions to common.py

---
+++ common.py
@@ -0,0 +1,33 @@
+from keras.preprocessing import image
+from sklearn.datasets import load_files
+from keras.utils import np_utils
+import numpy as np
+from tqdm import tqdm
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+
+def extract_xception(tensor):
+    from keras.applications.xception import Xception, preprocess_input
+    return Xception(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+
+def load_dataset(path):
+    data = load_files(path)
+    dog_files = np.array(data['filenames'])
+    dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
+    return dog_files, dog_targets
